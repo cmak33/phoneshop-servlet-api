23d5288ffdc463982ca4993167e02dcaa964b8ac
hahahalol9 <cmak7543@mail.ru>
2023-04-27 20:03:09 +0300

    Task 1.5 implemented ProductDao

--- src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
+++ src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
@@ -1,25 +1,52 @@
 package com.es.phoneshop.model.product;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ArrayListProductDao implements ProductDao {
+    private final List<Product> productList;
+    private long currentId = 1L;
+
+    public ArrayListProductDao() {
+        productList = Collections.synchronizedList(new ArrayList<>());
+    }
+
+    public ArrayListProductDao(List<Product> products) {
+        this();
+        products.forEach(this::save);
+    }
+
     @Override
     public Product getProduct(Long id) {
-        throw new RuntimeException("Not implemented");
+        return productList.stream()
+                .filter(product -> product.getId().equals(id))
+                .findAny()
+                .orElse(null);
     }
 
     @Override
     public List<Product> findProducts() {
-        throw new RuntimeException("Not implemented");
+        List<Product> unmodifiableProductList = productList.stream()
+                .filter(product -> product.getPrice() != null && product.getStock() > 0)
+                .toList();
+        return new ArrayList<>(unmodifiableProductList);
     }
 
     @Override
-    public void save(Product product) {
-        throw new RuntimeException("Not implemented");
+    public synchronized void save(Product product) {
+        if (product.getId() != null) {
+            delete(product.getId());
+        } else {
+            product.setId(currentId);
+            currentId++;
+        }
+        productList.add(product);
     }
 
     @Override
     public void delete(Long id) {
-        throw new RuntimeException("Not implemented");
+        Product productToDelete = getProduct(id);
+        productList.remove(productToDelete);
     }
 }
