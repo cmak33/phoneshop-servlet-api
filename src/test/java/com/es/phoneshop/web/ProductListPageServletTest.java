31e514504038e9363eef65afa15f103b002787e5
hahahalol9 <cmak7543@mail.ru>
2023-04-27 21:40:09 +0300

    Task 1.6 created test for ProductListPageServlet

--- src/test/java/com/es/phoneshop/web/ProductListPageServletTest.java
+++ src/test/java/com/es/phoneshop/web/ProductListPageServletTest.java
@@ -1,16 +1,20 @@
 package com.es.phoneshop.web;
 
+import com.es.phoneshop.model.product.ArrayListProductDao;
+import com.es.phoneshop.model.product.Product;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import jakarta.servlet.RequestDispatcher;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
@@ -24,18 +28,27 @@ public class ProductListPageServletTest {
     private HttpServletResponse response;
     @Mock
     private RequestDispatcher requestDispatcher;
+    @Mock
+    private ArrayListProductDao arrayListProductDao;
 
-    private ProductListPageServlet servlet = new ProductListPageServlet();
+    private final ProductListPageServlet servlet = new ProductListPageServlet();
 
     @Before
-    public void setup(){
+    public void setup() {
+        servlet.setProductDao(arrayListProductDao);
         when(request.getRequestDispatcher(anyString())).thenReturn(requestDispatcher);
     }
 
     @Test
     public void testDoGet() throws ServletException, IOException {
+        List<Product> productList = new ArrayList<>() {{
+            add(new Product());
+        }};
+        when(arrayListProductDao.findProducts()).thenReturn(productList);
+
         servlet.doGet(request, response);
 
+        verify(request).setAttribute("products", productList);
         verify(requestDispatcher).forward(request, response);
     }
 }
