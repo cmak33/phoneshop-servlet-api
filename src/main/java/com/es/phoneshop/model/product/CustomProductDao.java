9417245721e997b5523682ec2f299a6f04138c17
hahahalol9 <cmak7543@mail.ru>
2023-04-29 12:27:39 +0300

    Task 1.5 use ArrayList and ReadWriteLock in CustomProductDao

--- src/main/java/com/es/phoneshop/model/product/CustomProductDao.java
+++ src/main/java/com/es/phoneshop/model/product/CustomProductDao.java
@@ -1,15 +1,21 @@
 package com.es.phoneshop.model.product;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class CustomProductDao implements ProductDao {
     private final List<Product> productList;
+    private final Lock readLock;
+    private final Lock writeLock;
     private long currentId = 1L;
 
     public CustomProductDao() {
-        productList = Collections.synchronizedList(new ArrayList<>());
+        productList = new ArrayList<>();
+        ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+        readLock = readWriteLock.readLock();
+        writeLock = readWriteLock.writeLock();
     }
 
     public CustomProductDao(List<Product> products) {
@@ -19,34 +25,66 @@ public class CustomProductDao implements ProductDao {
 
     @Override
     public Product getProduct(Long id) {
-        return productList.stream()
-                .filter(product -> product.getId().equals(id))
-                .findAny()
-                .orElse(null);
+        Product result;
+        readLock.lock();
+        try {
+            result = productList.stream()
+                    .filter(product -> product.getId().equals(id))
+                    .findAny()
+                    .orElse(null);
+        } finally {
+            readLock.unlock();
+        }
+        return result;
     }
 
     @Override
     public List<Product> findProducts() {
-        List<Product> unmodifiableProductList = productList.stream()
-                .filter(product -> product.getPrice() != null && product.getStock() > 0)
-                .toList();
+        List<Product> unmodifiableProductList;
+        readLock.lock();
+        try {
+            unmodifiableProductList = productList.stream()
+                    .filter(product -> product.getPrice() != null && product.getStock() > 0)
+                    .toList();
+        } finally {
+            readLock.unlock();
+        }
         return new ArrayList<>(unmodifiableProductList);
     }
 
     @Override
-    public synchronized void save(Product product) {
-        if (product.getId() != null) {
-            delete(product.getId());
-        } else {
-            product.setId(currentId);
-            currentId++;
+    public void save(Product product) {
+        writeLock.lock();
+        try {
+            if (product.getId() != null) {
+                delete(product.getId());
+            } else {
+                product.setId(currentId);
+                currentId++;
+            }
+            productList.add(product);
+        } finally {
+            writeLock.unlock();
         }
-        productList.add(product);
     }
 
     @Override
     public void delete(Long id) {
         Product productToDelete = getProduct(id);
-        productList.remove(productToDelete);
+        writeLock.lock();
+        try {
+            productList.remove(productToDelete);
+        } finally {
+            writeLock.unlock();
+        }
+    }
+
+    public void clear() {
+        writeLock.lock();
+        try {
+            productList.clear();
+        } finally {
+            writeLock.unlock();
+        }
     }
 }
